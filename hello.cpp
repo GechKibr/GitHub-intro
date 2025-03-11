06803d1692ce52f496029f96fb9a1031612e3d8f
GechKibr <getachewkibr@gmail.com>
2025-03-11 08:25:47 +0000

    git tutorial with mosh

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"hello world"<<endl;
+int num;
+cout<<"enter the numder and dispaly :";
+cin>>num;
+cout<<"you enterd :"<<num;
+cout<<"this is c++ files ";
+return 0;
+}
